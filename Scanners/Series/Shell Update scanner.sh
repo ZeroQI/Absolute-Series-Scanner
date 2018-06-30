548f196663bb87d282c5fbff910d525ddb94ba6b
Nyaran <>
2018-06-30 14:43:24 +0200

    Fix shebang on shell updater

--- Scanners/Series/Shell Update scanner.sh
+++ Scanners/Series/Shell Update scanner.sh
@@ -1,2 +1,2 @@
-#/bin/sh
+#!/usr/bin/env sh
 wget "https://raw.githubusercontent.com/ZeroQI/Absolute-Series-Scanner/master/Scanners/Series/Absolute%20Series%20Scanner.py" -O "Absolute Series Scanner.py"
