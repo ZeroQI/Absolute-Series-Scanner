b6f2b32acf2e43e2dfa887845ed7e9f2b7ee1705
EndOfLine369 <dbrown369@gmail.com>
2018-08-28 21:25:28 -0500

    Handle when entries are missing in AniDB_op
    'KeyError: (0,)' crashes on 'cumulative_offset' calculation line
    https://github.com/ZeroQI/Absolute-Series-Scanner/issues/172

--- Scanners/Series/Absolute Series Scanner.py
+++ Scanners/Series/Absolute Series Scanner.py
@@ -829,21 +829,19 @@ def Scan(path, files, media, dirs, language=None, root=None, **kwargs): #get cal
                 #Log.info('type: {}, epno: {}, title: {}, ANIDB_RX.index(rx): {}'.format(type, epno, title_, ANIDB_RX.index(rx)))
                 if epno and not epno.isdigit() and len(epno)>1 and epno[:-1].isdigit():                                                                                    ### OP/ED with letter version Example: op2a
                   epno, offsetno = int(epno[:-1]), ord(epno[-1:])-ord('a')
-                elif epno and epno.isdigit():
-                  epno, offsetno = int(epno), 0
-                if   not index in AniDB_op:                                          AniDB_op [ index ]          = { epno:    offsetno }
-                elif not epno in AniDB_op[index] or offsetno>AniDB_op[index][epno]:  AniDB_op [ index ] [ epno ] = offsetno
-            Log.info("AniDB URL: {}, length: {}, AniDB_op: {}".format(ANIDB_HTTP_API_URL+id, len(anidb_str), AniDB_op))
+                  if   not index in AniDB_op:                                          AniDB_op[index]       = {epno: offsetno }
+                  elif not epno in AniDB_op[index] or offsetno>AniDB_op[index][epno]:  AniDB_op[index][epno] =        offsetno
+            Log.info("AniDB URL: '{}', length: {}, AniDB_op: {}".format(ANIDB_HTTP_API_URL+id, len(anidb_str), AniDB_op))
             
           ### OP/ED with letter version Example: op2a
           if not ep.isdigit() and len(ep)>1 and ep[:-1].isdigit():  ep, offset = int(ep[:-1]), ord(ep[-1:])-ord('a')
           else:                                                     offset = 0
           if anidb_xml is None:
-            if ANIDB_RX.index(rx) in AniDB_op:  AniDB_op [ ANIDB_RX.index(rx) ]   [ ep ] = offset # {101: 0 for op1a / 152: for ed2b} and the distance between a and the version we have hereep, offset                         = str( int( ep[:-1] ) ), offset + sum( AniDB_op.values() )                             # "if xxx isdigit() else 1" implied since OP1a for example... # get the offset (100, 150, 200, 300, 400) + the sum of all the mini offset caused by letter version (1b, 2b, 3c = 4 mini offset)
-            else:                               AniDB_op [ ANIDB_RX.index(rx) ] = { ep:    offset }
-          cumulative_offset = sum( [ AniDB_op [ ANIDB_RX.index(rx) ][x] for x in Dict(AniDB_op, ANIDB_RX.index(rx), default={0:0}) if x<ep ] )
-          ep = ANIDB_OFFSET [ ANIDB_RX.index(rx) ] + int(ep) + offset + cumulative_offset    # Sum of all prior offsets
-          #Log.info('ep type offset: {}, ep: {}, offset: {}, cumulative_offset: {}, final ep number: {}'.format(ANIDB_OFFSET [ ANIDB_RX.index(rx) ], ep, offset, cumulative_offset, ep))
+            if ANIDB_RX.index(rx) in AniDB_op:  AniDB_op[ANIDB_RX.index(rx)]   [ep] = offset # {101: 0 for op1a / 152: for ed2b} and the distance between a and the version we have hereep, offset                         = str( int( ep[:-1] ) ), offset + sum( AniDB_op.values() )                             # "if xxx isdigit() else 1" implied since OP1a for example... # get the offset (100, 150, 200, 300, 400) + the sum of all the mini offset caused by letter version (1b, 2b, 3c = 4 mini offset)
+            else:                               AniDB_op[ANIDB_RX.index(rx)] = {ep:   offset}
+          cumulative_offset = sum( [ AniDB_op[ANIDB_RX.index(rx)][x] for x in Dict(AniDB_op, ANIDB_RX.index(rx), default={0:0}) if x<ep and ANIDB_RX.index(rx) in AniDB_op and x in AniDB_op[ANIDB_RX.index(rx)] ] )
+          ep = ANIDB_OFFSET[ANIDB_RX.index(rx)] + int(ep) + offset + cumulative_offset    # Sum of all prior offsets
+          #Log.info('ep type offset: {}, ep: {}, offset: {}, cumulative_offset: {}, final ep number: {}'.format(ANIDB_OFFSET[ANIDB_RX.index(rx)], ep, offset, cumulative_offset, ep))
         add_episode_into_plex(media, file, root, path, show, int(season), int(ep), title, year, int(ep2) if ep2 and ep2.isdigit() else int(ep), rx, tvdb_mapping, unknown_series_length, offset_season, offset_episode, mappingList)
         break
     if match: continue  # next file iteration
